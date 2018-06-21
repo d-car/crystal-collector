aed405437d2cf0c490519916876efa405a3933ea
d-car <danielscarstensen@gmail.com>
2018-06-21 02:42:11 -0500

    fix crystal value loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,31 @@ var count = 0;
 var crystalNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var wins = 0;
 var loss = 0;
+var compNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     
 
 function randoNumber()
 {
-    var compNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    compNumber
     return compNumber
 
+}
+
+function display()
+{
+    $("#rando").text("Number to Guess: " + randoNumber());
+    $("#scoreCounter").text("Crystal Count: " + count);  
 }
 
 
 
 function runGame() 
 {
+    display();
+    
     var compNumber = randoNumber();
-    $("#rando").text("Number to Guess: " + compNumber);
-    $("#scoreCounter").text("Crystal Count: " + count);
+    // $("#rando").text("Number to Guess: " + compNumber);
+    // $("#scoreCounter").text("Crystal Count: " + count);
     $("#wins").text("Wins: " + wins);
 
     for (var i = 0; i < 4; i++) 
@@ -27,7 +36,14 @@ function runGame()
     var crystalPic = $("<img>");
     crystalPic.addClass("crystal-image");
     crystalPic.attr("src", "../crystal-collector/assets/images/crystal1.jpg");
-    crystalPic.attr("data-crystalvalue", Math.floor(Math.random() * crystalNumber.length));
+    
+    function randoCrystal()
+        {
+        crystalPic.attr("data-crystalvalue", Math.floor(Math.random() * crystalNumber.length));    
+        }
+    
+    randoCrystal();
+    
     $("#crystalBox").append(crystalPic);
   
     }
@@ -42,16 +58,18 @@ function runGame()
 
         $("#scoreCounter").text("Crystal Count: " + count);
 
-        alert(crystalValue);
+        // alert(crystalValue);
 
     if (compNumber === count) {
         alert("You Win!");
         wins++;
         reset();
+        // display();
     } else if (compNumber < count) {
         alert("You Lose!");
         loss++;
         reset();
+        // display();
         }
     function reset()
         {
@@ -59,8 +77,12 @@ function runGame()
         crystalValue = 0;
         crystalPic.attr("data-crystalvalue", 0);
         randoNumber();
+        randoCrystal();
+        display();
+        }
+    
         
-    }
+    
     });
 
     }
