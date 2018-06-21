6e8ab398a96bd52ac67df6e767f65fdf0bde3954
d-car <danielscarstensen@gmail.com>
2018-06-21 02:18:27 -0500

    almost working. fix reset function

---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+
+var count = 0;
+var crystalNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var wins = 0;
+var loss = 0;
+    
+
+function randoNumber()
+{
+    var compNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    return compNumber
+
+}
+
+
+
+function runGame() 
+{
+    var compNumber = randoNumber();
+    $("#rando").text("Number to Guess: " + compNumber);
+    $("#scoreCounter").text("Crystal Count: " + count);
+    $("#wins").text("Wins: " + wins);
+
+    for (var i = 0; i < 4; i++) 
+    {
+        
+    var crystalPic = $("<img>");
+    crystalPic.addClass("crystal-image");
+    crystalPic.attr("src", "../crystal-collector/assets/images/crystal1.jpg");
+    crystalPic.attr("data-crystalvalue", Math.floor(Math.random() * crystalNumber.length));
+    $("#crystalBox").append(crystalPic);
+  
+    }
+
+    $(".crystal-image").on("click", function() 
+    
+    {
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        count += crystalValue;
+
+        $("#scoreCounter").text("Crystal Count: " + count);
+
+        alert(crystalValue);
+
+    if (compNumber === count) {
+        alert("You Win!");
+        wins++;
+        reset();
+    } else if (compNumber < count) {
+        alert("You Lose!");
+        loss++;
+        reset();
+        }
+    function reset()
+        {
+        count = 0;
+        crystalValue = 0;
+        crystalPic.attr("data-crystalvalue", 0);
+        randoNumber();
+        
+    }
+    });
+
+    }
+    
+
+runGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
